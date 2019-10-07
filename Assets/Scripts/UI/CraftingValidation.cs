fb3b1c826e6f8a403420355a6e7cb4fd8e68c081
Ralph SotheGarnier <ralph@soethe.fr>
2019-10-07 15:23:42 +0200

    Crafted items

--- Assets/Scripts/UI/CraftingValidation.cs
+++ Assets/Scripts/UI/CraftingValidation.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class CraftingValidation : MonoBehaviour {
 	
-	public int clothNb;
-	public int plankNb;
-	public int ropeNb;
-	public int seaweedNb;
-	public int stickNb;
-	public int tarpaulinNb;
-	public int metalNb;
-	public int sumItems;
-	public string nameTag;
+	private int clothNb;
+	private int plankNb;
+	private int ropeNb;
+	private int seaweedNb;
+	private int stickNb;
+	private int tarpaulinNb;
+	private int metalNb;
+	private int sumItems;
+	private string nameTag;
 
-	public GameObject[] previsu;
+	public GameObject[] item;
 
 	private Crafting crafting;
 	private Inventory inventory;
+	private GameObject previsu;
 
 	public void CheckCraftable () {
 		//reset the numbers
@@ -28,7 +29,8 @@ public class CraftingValidation : MonoBehaviour {
 		stickNb = 0;
 		tarpaulinNb = 0;
 		metalNb = 0;
-
+		previsu = GameObject.FindGameObjectWithTag("Previsu");
+		Destroy(previsu);
 		inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
 		crafting = GameObject.FindGameObjectWithTag("Player").GetComponent<Crafting>();
 
@@ -72,7 +74,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[0], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_SeaweedWig"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -83,7 +85,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[1], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_Poncho"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -94,7 +96,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[2], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_Hat"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -105,7 +107,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[3], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_PonchoPermeable"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -116,7 +118,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[4], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_HatPermeable"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -127,7 +129,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[5], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_Spear"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -138,7 +140,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[6], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_Net"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -149,7 +151,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[7], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_SurvivalRaft"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -160,7 +162,7 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[8], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_PaddleSmall"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
@@ -171,10 +173,222 @@ public class CraftingValidation : MonoBehaviour {
 			{
 				if (inventory.isFull[i] == false)
 				{
-					Instantiate(previsu[9], inventory.slots[i].transform, false);
+					Instantiate(Resources.Load("Previsu_SailSmall"), inventory.slots[i].transform, false);
 					break;
 				}
 			}
 		}
+	}
+
+	public void Craft () {
+
+		//reset the numbers
+		clothNb = 0;
+		plankNb = 0;
+		ropeNb = 0;
+		seaweedNb = 0;
+		stickNb = 0;
+		tarpaulinNb = 0;
+		metalNb = 0;
+		previsu = GameObject.FindGameObjectWithTag("Previsu");
+		Destroy(previsu);
+		inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+		crafting = GameObject.FindGameObjectWithTag("Player").GetComponent<Crafting>();
+
+		//check if something is craftable
+		for (int i = 0; i<crafting.slots.Length; i++) {
+			if (crafting.isFull[i] == true) {
+				nameTag = crafting.slots[i].transform.GetChild(0).gameObject.tag;
+				Debug.Log(nameTag);
+				//increment the numbers of items
+				if (nameTag == "Cloth") {
+					clothNb = clothNb + 1;
+				}
+				else if (nameTag == "Plank") {
+					plankNb = plankNb + 1;
+				}
+				else if (nameTag == "Rope") {
+					ropeNb= ropeNb + 1;
+				}
+				else if (nameTag == "Seaweed") {
+					seaweedNb = seaweedNb + 1;
+				}
+				else if (nameTag == "Stick") {
+					stickNb = stickNb + 1;
+				}
+				else if (nameTag == "Tarpaulin") {
+					tarpaulinNb = tarpaulinNb + 1;
+				}
+				else if (nameTag == "Scrap")
+				{
+					metalNb = metalNb + 1;
+				}
+			}
+		}
+		//to have exact recipes, count how many items in total
+		sumItems = clothNb + plankNb + ropeNb + seaweedNb + stickNb + tarpaulinNb + metalNb;
+
+		//craft the item
+		if (seaweedNb==3 && sumItems==3) //seaweed wig
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("SeaweedWigButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (clothNb == 2 && sumItems == 2) //poncho
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("PonchoButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (clothNb == 1 && sumItems == 1) //hat
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("HatButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (tarpaulinNb == 1 && ropeNb == 1 && sumItems == 2) //permeable poncho
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("PonchoPButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (tarpaulinNb == 1 && sumItems == 1) //permeable hat
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("HatPButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (stickNb == 1 && metalNb == 1 && sumItems == 2) //spear
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("SpearButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (ropeNb == 2 && sumItems == 2) //net
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("NetgButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (plankNb == 4 && sumItems == 4) //survival raft
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("RaftSButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (plankNb == 1 && sumItems == 1) //small paddle
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("PaddleSButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+		else if (tarpaulinNb == 2 && ropeNb == 1 && sumItems == 3) //small sail
+		{
+			for (int i = 0; i < inventory.slots.Length; i++)
+			{
+				if (inventory.isFull[i] == false)
+				{
+					inventory.isFull[i] = true;
+					Instantiate(Resources.Load("SailSButton"), inventory.slots[i].transform, false);
+					break;
+				}
+			}
+			for (int i = 0; i < crafting.slots.Length; i++)
+			{
+				Destroy(crafting.slots[i].transform.GetChild(0).gameObject);
+			}
+		}
+
 	}
 }
